a9b017307e704e6ad1a1195559c495105a172940
Khayratdinov Nurpolat <Khayratdinov.np@gmail.com>
2021-11-16 19:02:03 +0500

    Worked on templates and  Comments added

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from movies.views import MoviesListView, MovieDetailView
+from movies.views import MoviesListView, MovieDetailView, AddReview
 
 
 urlpatterns = [
     path('', MoviesListView.as_view(), name="movie_list"),
-    path('<slug:slug>/', MovieDetailView.as_view(), name="movie_detail")
+    path('<slug:slug>/', MovieDetailView.as_view(), name="movie_detail"),
+    path("review/<int:pk>/", AddReview.as_view(), name="add_review"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
+from movies.forms import ReviewForm
+from django.shortcuts import redirect
 
 from movies.models import Movie
 # Create your views here.
@@ -15,4 +16,18 @@ class MovieDetailView(DetailView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     slug_field = "slug"
+
+class AddReview(View):
+
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
 
